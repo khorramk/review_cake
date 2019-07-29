cbf152ae38229a0e6a430bd1354bdd6027d62681
Khorram Khondkar <Khorram.Khondkar@bbtravel.local>
2019-07-29 16:29:47 +0100

    code formatted for readable purposes

--- resources/views/comments/comment.blade.php
+++ resources/views/comments/comment.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app') 
 
 @section('content')
-        <h1>{{$review->content}}</h1>
-        <form action="{{action('CommentController@store')}}" method="POST">
-            @csrf
+    <h1>{{$review->content}}</h1>
+    <form action="{{action('CommentController@store')}}" method="POST">
+        @csrf
         <input type="hidden" name="review_id" value="{{$review->id}}">
-            <textarea name="comment" id="" cols="30" rows="10"></textarea>
-            <input type="submit" value="add comments">
-        </form>
+        <textarea name="comment" id="" cols="30" rows="10"></textarea>
+        <input type="submit" value="add comments">
+    </form>
 @endsection
