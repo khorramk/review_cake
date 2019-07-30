7c7dcc53ab55c9ead65ea1ddaa9f116a15602a71
Khorram Khondkar <Khorram.Khondkar@bbtravel.local>
2019-07-30 09:25:32 +0100

    remove empty method called index

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -8,17 +8,6 @@ use App\Comment;
 class CommentController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-       
-    }
-
-    /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
