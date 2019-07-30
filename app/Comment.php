3fc71c3157210ee6042832014c90cd5c0ba71223
Khorram Khondkar <Khorram.Khondkar@bbtravel.local>
2019-07-30 16:59:02 +0100

    removed default relationship values

--- app/Comment.php
+++ app/Comment.php
@@ -6,13 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
-    
-    public $attributes = [
-        'review_id' => 1,
-        'user_id' => 1,
-    ];
-
     public function review()
     {
         return $this->belongsTo('App\Review');
