cbdb2c500bcd1ebeeb2df330fca7eb8f31578367
Khorram Khondkar <Khorram.Khondkar@bbtravel.local>
2019-08-01 16:09:08 +0100

    issues fixed for route binding

--- database/migrations/2019_07_22_133427_create_reviews_table.php
+++ database/migrations/2019_07_22_133427_create_reviews_table.php
@@ -15,7 +15,7 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('reviews')->nullable();
+            $table->string('reviews');
             $table->timestamps();
         });
     }
--- resources/views/review-vue/commentCreate.blade.php
+++ resources/views/review-vue/commentCreate.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.app')
 @section('content')
-<comment-create></comment-create>
+<comment-create :review-id="{{$reviewId}}"></comment-create>
 @endsection
