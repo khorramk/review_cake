d5e279e9411c2d559185d6493f44b2370d8ed917
khorramkho <52155201+khorramkho@users.noreply.github.com>
2019-08-02 15:21:43 +0100

    removed from web file and added to the api all the logics

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,19 @@ Route::apiResources([
     '/reviews' => 'ReviewController',
 ]);
 
+Route::get('/reviews/create', function (\App\Review $review)
+{
+    return View::make('review-vue.create');
+});
+Route::get('/comments/create/{review}', function (\App\Review $review)
+{
+    return View::make('review-vue.commentCreate')->withReviewId($review->id);
+});
+Route::get('/comments/{comment}/edit', function (\App\Comment $comment)
+{
+    return view('comments.edit')->withCommentId($comment->id);
+});
+Route::get('/reviews/{review}/edit', function(\App\Review $review)
+{
+    return view('review-vue.reviewEdit')->withReview($review);
+});
--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,6 @@
 */
 Route::get('/', 'ReviewController@welcome');
 Auth::routes();
-Route::get('/reviews/create', function (\App\Review $review)
-{
-    return View::make('review-vue.create');
-});
-Route::get('/comments/create/{review}', function (\App\Review $review)
-{
-    return View::make('review-vue.commentCreate')->withReviewId($review->id);
-});
-Route::get('/comments/{comment}/edit', function (\App\Comment $comment)
-{
-    return view('comments.edit')->withCommentId($comment->id);
-});
-Route::get('/reviews/{review}/edit', function(\App\Review $review)
-{
-    return view('review-vue.reviewEdit')->withReview($review);
-});
+
 
 
