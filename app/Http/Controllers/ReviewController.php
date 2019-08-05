e2f14349ebbea43c776ede75e65756623d8f3a1d
khorramkho <52155201+khorramkho@users.noreply.github.com>
2019-08-05 15:36:46 +0100

    added missing docblock to the controllers

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -8,10 +8,21 @@ use App\Review;
 use Illuminate\Http\Request;
 class ReviewController extends Controller
 {
+    /**
+     * create reviews from scratch from form submission
+     * @param Model $review
+     * @return \Illuminate\http\Response 
+     */
     public function create(\App\Review $review)
     {
+        
         return view('review-vue.create');
     }
+    /**
+     * edits reviews
+     * @param Model $review
+     * @return \Illuminate\http\Response
+     */
     public function edit(\App\Review $review)
     {
         return view('review-vue.reviewEdit')->withReview($review);
